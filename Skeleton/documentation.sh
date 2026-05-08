84596cf3cfc7d6f4e09d8c8078ffd4009c495369
HeapHeapHooray <heapheaphooray@hotmail.com>
2026-05-08 11:55:48 -0300

    Changes.

--- Skeleton/documentation.sh
+++ Skeleton/documentation.sh
@@ -7,4 +7,4 @@ fi
 
 ralph "
 Run 1-Documentation/per-function.sh .
-If there is any listed function that is not defined in 1-Documentation/ARCHITECTURE.md, then task is not complete." --allow-all
+If there is any listed function that is not defined in 1-Documentation/ARCHITECTURE.md, then task is not complete."
